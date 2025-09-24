c3aa902b9c8bd52b662f9f6cc882e797cfdb5043
mariamtantawy768-stack <mariamtantawy768@gmail.com>
2025-09-24 11:29:06 +0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,31 +1,28 @@
-let addTask  = document.getElementById("two") ;
-let firstInput = document.getElementById('one');
-let  tasksBox   = document.getElementById('tasks');
-addTask.value = "Add Task";
+let firstInput = document.getElementById("one");
+let addTask = document.getElementById("two");
+let tasksBox = document.getElementById("tasks");
 
 addTask.addEventListener('click' , () => {
+
     let val1 = firstInput.value.trim();
-    
-    if(val1 !== "") {
-        let task = document.createElement("p")
+
+    if (val1 !== "") {
+        let task = document.createElement("p");
+
         task.textContent = val1;
-
-        tasksBox.appendChild(task);
         task.classList.add("task-item")
+        tasksBox.appendChild(task);
 
-        let dltbtn = document.createElement("Button")
-        dltbtn.textContent = "Delete";
-
-        dltbtn.addEventListener("click", () => {
-            task.remove();
-        })
-        task.appendChild(dltbtn);
+        let submit = document.createElement("input");
+        submit.type = "checkbox" ;
         
-        dltbtn.classList.add("dlt-item")
-        firstInput.value = "" ;
-        dltbtn.value = "";
+
+        submit.classList.add("smt-item");
+        submit.addEventListener("change", () => {
+            task.classList.toggle("done", submit.checked);
+         })
+        task.appendChild(submit);
     }
+    
+    firstInput.value = "";
 })
-
-
-
